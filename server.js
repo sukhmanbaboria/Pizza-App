155b180ee218769a2d991b97ed059503cbda8fc9
SUKHMAN SINGH <baboriasukhman@gmail.com>
2022-03-03 19:09:29 +0530

    auth system

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("express-flash");
+const exp = require("constants");
 const MongoDbStore = require("connect-mongo")(session);
+const passport = require("passport");
 
 // Database connection
 const url = "mongodb://localhost/pizza";
@@ -44,16 +46,25 @@ app.use(
   })
 );
 
+//Passpost config
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
+
 //Assets
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.urlencoded({ extented: false }));
+app.use(express.json());
 
-// gobal Middleware
+// Gobal Middleware
 app.use((req, res, next) => {
-res.locals.session = req.session
-next()
-})
+  res.locals.session = req.session;
+  res.locals.user = req.user;
+  next();
+});
 // set Template engine:-
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
